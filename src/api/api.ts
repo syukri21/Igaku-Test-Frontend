361e4ad1c50ddaea4c46a593cbb7191049e19460
myari211 <syukrihsb148@gmail.com>
2020-06-14 12:54:50 +0700

    fix

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from "axios"
 
 class Api {
     private static instance: Api
-    private apiUrl: string | undefined = process.env.REACT_APP_API_URI
+    private apiUrl: string | undefined = "http://ec2-18-191-129-34.us-east-2.compute.amazonaws.com:3030"
     private token?: string | null
 
     private constructor() {}
